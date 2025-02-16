25006608a6eb56494c027833ceb6f2ffcc1cfd31
ipipi2024 <ipulepipi60@gmail.com>
2025-02-16 16:50:40 -0500

    added rotate method

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -42,6 +42,78 @@ public class CircularLinkedList {
         size++;
     }
 
+    //method for adding last
+    public void addAtLast(int data) {
+        Node newNode = new Node(data, null);
+        if(size == 0) {
+            tail = newNode;
+            tail.next = tail;
+        }else{
+            newNode.next = tail.next; // point next of newnode to head
+            tail.next = newNode; //prev tail point to newNode since it is now second last
+            tail = newNode; //new tail to be at newNode
+        }
+
+        size++;
+    }
+
+    //method to removefirst
+    public int removeFirst() {
+        if(size == 0) {
+            System.out.println("List is empty. Cannot remove");
+            return -1;
+        }
+
+        int removedItem = tail.next.data;
+
+        if (size == 1) {
+            tail = null;
+        }else{
+            tail.next = tail.next.next;
+        }
+
+        size--;
+        return removedItem;
+
+    }
+
+    //remove first
+    public int removeLast() {
+        if(size == 0) {
+            System.out.println("List is empty. Cannot remove");
+            return -1;
+        }
+
+        int removedItem = tail.data;
+
+        if (size == 1) {
+            tail = null;
+        }else{
+            Node currentNode = tail.next; //head node
+            while (currentNode.next != tail) { //current
+                currentNode = currentNode.next;
+            }
+            //current node is now the previous node to tail
+            currentNode.next = tail.next; 
+            tail = currentNode;
+        }
+        size--;
+        return removedItem;
+
+    }
+
+    //method to rotate list
+    public void rotate () {
+        if (size <= 1) {
+            //essentially nothing to rotate
+            return;
+        }
+        //just set tail to the next of tail 
+        //so head begins becomes tail
+        //there tail becomes head
+        tail = tail.next;
+    }
+
     public void printList() {
         // Handle empty list
         if (tail == null) {
@@ -63,7 +135,14 @@ public class CircularLinkedList {
         circularLinkedList.addAtFirst(10);
         circularLinkedList.addAtFirst(20);
         circularLinkedList.addAtFirst(30);
-
         circularLinkedList.printList();
+        circularLinkedList.rotate();
+        circularLinkedList.printList();
+        circularLinkedList.rotate();
+        circularLinkedList.printList();
+        
+      
+        
+
     }
 }
